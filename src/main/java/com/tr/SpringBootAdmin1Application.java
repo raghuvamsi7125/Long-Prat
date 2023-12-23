aa3ad85fdd8d14ae1945a67d5fe99ae0b720d789
Raghu <raghuvamsi836@gmail.com>
2023-12-23 20:55:45 +0530

    commetted

---
+++ src/main/java/com/tr/SpringBootAdmin1Application.java
@@ -0,0 +1,15 @@
+package com.tr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootAdmin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdmin1Application.class, args);
+	}
+
+}
